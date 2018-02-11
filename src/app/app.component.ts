3cdfeb20d5cc795059d03ab58bd649521f26ad13
Akhil Mukkamala <akhil.mukkamala@gmail.com>
2018-02-12 02:42:12 +0530

    Event Binding Explained

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  
-  title = 'Angular Bindings';
+  title = "Angular Bindings";
   serverId: Number = 10;
-  serverStatus: String = 'Offline';
+  serverStatus: String = "Offline";
   allowNewServer: Boolean = false;
-
+  serverCreationStatus: String = "No Server was Created";
   anotherServerId: Number = 11;
-  anotherServerStatus: String = 'Online';
+  anotherServerStatus: String = "Online";
+  serverName: String = '';
 
   getServerStatus() {
     return this.anotherServerStatus;
   }
 
-// Property Binding
+  // Property Binding
 
-constructor() {
-  setTimeout( () => {
-    this.allowNewServer = true;
-  }, 2000);
-}
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {
+  }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was Created';
+  }
+
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 
 }
