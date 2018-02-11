b750c4efc7ac78ea1e8d3e00493e4be9020454af
Akhil Mukkamala <akhil.mukkamala@gmail.com>
2018-02-12 01:55:56 +0530

    String Interpolation Explained

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'Angular Bindings';
+  serverId: Number = 10;
+  serverStatus: String = 'Offline';
+
+  anotherServerId: Number = 11;
+  anotherServerStatus: String = 'Online';
+
+  getServerStatus() {
+    return this.anotherServerStatus;
+  }
+
+
 }
